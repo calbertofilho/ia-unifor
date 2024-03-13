76fd9b4133dbb1389509f60aa722244caba2a8e7
Carlos Alberto Filho <calbertofilho@gmail.com>
2024-03-12 21:13:54 -0300

    Update

--- 20240228-02_tsp.py
+++ 20240228-02_tsp.py
@@ -1,32 +1,48 @@
-# Traveling Salesman Problem 2
+# Traveling Salesman Problem '2D'
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-def perturb(x, e):
-    return x
+def perturb(x,e):
+    return np.random.uniform(low=x-e, high=x+e)
 
 def f(x):
     return np.exp(-x ** 2)
 
+x_axis = np.linspace(-2, 2, 1000)
 x_otimo = np.random.uniform(low=-2, high=2)
 f_otimo = f(x_otimo)
 
-e = 1
+delay=.5
+e = .1
 max_it = 1000
-max_vizinhos = 10
+max_vizinhos = 20
 i = 0
 melhoria = True
+
+plt.plot(x_axis, f(x_axis))
+plt.grid()
+plt.suptitle('Realizando busca...', fontsize = 12)
+plt.scatter(x_otimo, f_otimo, color='green', s=120, marker='x', linewidth=1)
 
 while i<max_it and melhoria:
     melhoria = False
     for j in range(max_vizinhos):
         x_candidato = perturb(x_otimo, e)
         f_candidato = f(x_candidato)
+        if(f_candidato>f_otimo):
+            melhoria = True
+            x_otimo = x_candidato
+            f_otimo = f_candidato
+            plt.pause(delay)
+            plt.title("x = "+"{:.4f}".format(x_otimo)+"\nf(x) = "+"{:.4f}".format(f_otimo), fontsize = 8)
+            plt.scatter(x_otimo, f_otimo, color='black', s=20, marker='x', linewidth=1)
+            break
+    i+=1
 
-plt.scatter(x_otimo, f_otimo, color='red', s=90, marker='x')
-x_axis = np.linspace(-2, 2, 1000)
-plt.plot(x_axis, f(x_axis))
-plt.grid()
-#plt.show()
+plt.pause(delay)
+plt.scatter(x_otimo, f_otimo, color='red', s=120, marker='x', linewidth=1)
+
+plt.suptitle('Solução ótima encontrada!', fontsize = 12)
 plt.savefig('20240228-02_tsp.png')
+plt.show()
--- av1.py
+++ av1.py
@@ -4,44 +4,50 @@ import matplotlib.pyplot as plt
 
 def f(x1, x2):
     # return (x1 ** 2 + x2 ** 2)
-    return (np.exp(-(x1 ** 2 + x2 ** 2)) + 2 * np.exp(-((x1 - 1.7) ** 2 + (x2 - 1.7) ** 2)))
+    # return (np.exp(-(x1 ** 2 + x2 ** 2)) + 2 * np.exp(-((x1 - 1.7) ** 2 + (x2 - 1.7) ** 2)))
     # return (-20 * np.exp(-0.2 * np.sqrt(0.5 * (x1 ** 2 + x2 ** 2)))) - np.exp(0.5 * (np.cos(2 * np.pi * x1) + np.cos(2 * np.pi * x2))) + 20 + np.exp(1)
     # return ((x1 ** 2 - 10 * np.cos(2 * np.pi * x1) + 10) + (x2 ** 2 - 10 * np.cos(2 * np.pi * x2) + 10))
     # return (((x1 * np.cos(x1)) / 20) + 2 * np.exp(-(x1 ** 2) - ((x2 - 1) ** 2)) + 0.01 * x1 * x2)
     # return ((x1 * np.sin(4 * np.pi * x1)) - (x2 * np.sin((4 * np.pi * x2) + np.pi)) + 1)
     # return ((-np.sin(x1) * np.sin((x1 ** 2)/np.pi) ** (2 * 10)) - (np.sin(x2) * np.sin((x2 ** 2)/np.pi) ** (2 * 10)))
-    # return ((-(x2 + 47)) * np.sin(np.sqrt(np.abs((x1 / 2) + (x2 + 47))))) - (x1 * np.sin(np.sqrt(np.abs(x1 - (x2 + 47)))))
+    return ((-(x2 + 47)) * np.sin(np.sqrt(np.abs((x1 / 2) + (x2 + 47))))) - (x1 * np.sin(np.sqrt(np.abs(x1 - (x2 + 47)))))
 
 # x1 = np.linspace(-100, 100, 1000)
-x1 = np.linspace(start=[-2, -2], stop=[4, 5], num=1000)
+# x1, x2 = np.linspace(start=[-2, -2], stop=[4, 5], num=1000, axis=1)
 # x1 = np.linspace(-8, 8, 1000)
 # x1 = np.linspace(-5.12, 5.12, 1000)
 # x1 = np.linspace(-10, 10, 1000)
 # x1 = np.linspace(-1, 3, 1000)
 # x1 = np.linspace(0, np.pi, 1000)
-# x1 = np.linspace(-200, 20, 1000)
-X1,X2 = np.meshgrid(x1, x1)
+x1 = np.linspace(-200, 20, 1000)
+
+X1, X2 = np.meshgrid(x1, x1)
+# X1, X2 = np.meshgrid(x1, x2)
+
 Y = f(X1, X2)
 
 # x1_cand, x2_cand = 50, 50
-x1_cand, x2_cand = 0, -1
+# x1_cand, x2_cand = 0, -1
 # x1_cand, x2_cand = 0, -2
 # x1_cand, x2_cand = 1, -5
 # x1_cand, x2_cand = -2, 0
 # x1_cand, x2_cand = -1, 0
 # x1_cand, x2_cand = 3, 2
-# x1_cand, x2_cand = 3, 2
+x1_cand, x2_cand = 3, -2
 f_cand = f(x1_cand, x2_cand)
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.plot_surface(X1, X2, Y, rstride=10, cstride=10, alpha=0.6, cmap='jet')
+surface = ax.plot_surface(X1, X2, Y, rstride=10, cstride=10, alpha=0.6, cmap='jet')
 ax.scatter(x1_cand, x2_cand, f_cand, marker='x', s=90, linewidth=3, color='red')
 
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+ax.set_xlabel('valores x')
+ax.set_ylabel('valores y')
+ax.set_zlabel('valores z')
 ax.set_title('f(x1, x2)')
+# ax.view_init(elev=24., azim=-66., roll=0.)   # Problem 2
+ax.view_init(elev=30., azim=160., roll=0.)   # Problem 8
+plt.colorbar(surface)
 plt.tight_layout()
 
 # plt.savefig('av1-problem1.png')
@@ -51,6 +57,6 @@ plt.tight_layout()
 # plt.savefig('av1-problem5.png')
 # plt.savefig('av1-problem6.png')
 # plt.savefig('av1-problem7.png')
-# plt.savefig('av1-problem8.png')
+plt.savefig('av1-problem8.png')
 
 plt.show()
