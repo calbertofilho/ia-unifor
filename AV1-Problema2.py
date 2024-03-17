8170da70814a9e6ba131c597e173b278c1722e03
Carlos Alberto Filho <calbertofilho@gmail.com>
2024-03-17 19:19:16 -0300

    Update 17-03 Conclusao da funcao h(x)

--- AV1-Problema2.py
+++ AV1-Problema2.py
@@ -24,35 +24,78 @@ import pandas as pd
 import random as rd
 import matplotlib.pyplot as plot
 
-entrada = num.array([5, 1, 4, 2, 6, 1, 4, 7], dtype=int)
-matriz = num.zeros((8, 8), dtype=int)
-for posicao in range(len(entrada)):
-    coluna = posicao
-    linha = len(entrada) - entrada[posicao]
-    matriz[linha][coluna] = 1
-print('Entrada')
-print(matriz)
-print('')
-
-for pos in range(len(entrada)):
-    col = pos
-    lin = len(entrada) - entrada[pos]
-
-    rainha = num.zeros((8, 8), dtype=int)
-    rainha[lin][col] = 1
-
-    movimentos = num.zeros((8, 8), dtype=int)
-    for iter in range(len(entrada)):
-        movimentos[lin][iter] = 1    # linha
-        movimentos[iter][col] = 1    # coluna
-        print(str(lin)+' '+str(col))
-        # num.fill_diagonal(movimentos[-lin:, -col:], 9) # diag ascendente
-        num.fill_diagonal(movimentos[lin:, col:], 5) # diag descendente
-
-    print('Posição da Rainha '+str(pos+1)+' = ('+str(entrada[pos])+', '+str(pos+1)+')')
-    print(rainha)
-    print('Movimentos da Rainha '+str(pos+1))
-    print(movimentos)
+def h(entrada):
+    # # Matriz recebida como entrada
+    # matriz = num.zeros((8, 8), dtype=int)
+    # for posicao in range(len(entrada)):
+    #     coluna = posicao
+    #     linha = len(entrada) - entrada[posicao]
+    #     matriz[linha][coluna] = 1
+    # print('Entrada')
+    # print(matriz)
     # print('')
-    # print(num.identity(8, dtype=int))
-    # print(num.fliplr(num.identity(8, dtype=int)))
+
+    rainhas = [0] * len(entrada)
+    movimentos = [0] * len(entrada)
+
+    for pos in range(len(entrada)):
+        col = pos
+        lin = len(entrada) - entrada[pos]
+
+        # Posicao da rainha que esta sendo analisada
+        rainha = num.zeros((8, 8), dtype=int)
+        rainha[lin][col] = 1
+        rainhas[pos] = rainha
+        # print('Posição da Rainha '+str(pos+1)+' = ('+str(entrada[pos])+', '+str(pos+1)+')')
+        # print(rainha)
+
+        movimento_rainha = num.zeros((8, 8), dtype=int)
+        diag_ascendente = [-1] * 8
+        diag_descendente = [-1] * 8
+        for iter in range(len(entrada)):
+            movimento_rainha[lin][iter] = 1                          # possibilidade de movimentos para a linha
+            movimento_rainha[iter][col] = 1                          # possibilidade de movimentos para a coluna
+            if 0 < (entrada[pos] + iter - pos) <= len(entrada): # vetor com a diagonal ascendente
+                diag_ascendente[iter] = entrada[pos] + iter - pos
+            if 0 < (entrada[pos] - iter + pos) <= len(entrada): # vetor com a diagonal descendente
+                diag_descendente[iter] = entrada[pos] - iter + pos
+        for i in range(len(diag_ascendente)):                        # possibilidade de movimentos para a diagonal ascendente
+            column = i
+            if not(diag_ascendente[i] == -1):
+                row = len(diag_ascendente) - diag_ascendente[i]
+                movimento_rainha[row][column] = 1
+        for j in range(len(diag_descendente)):                       # possibilidade de movimentos para a diagonal descendente
+            c = j
+            if not(diag_descendente[j] == -1):
+                r = len(diag_descendente) - diag_descendente[j]
+                movimento_rainha[r][c] = 1
+
+        movimentos[pos] = movimento_rainha
+
+    # print('Movimentos da Rainha 5')
+    # print(movimentos[4])
+        
+    # somar o primeiro com:
+        # o ultimo
+        # o penultimo
+        # o antepenultimo
+        # até chegar no primeiro
+    # verificar as coincidências (valor = 2) e retornar quantas vezes se repetem
+    res = 0
+    for i in range(len(movimentos)):
+        for j in reversed(range(len(movimentos))):
+            analise = num.zeros((8, 8), dtype=int)
+            if (i < j):
+                analise = num.add(rainhas[i], movimentos[j])
+                res += num.any(analise[:] == 2)                
+    return res
+
+if __name__ == '__main__':
+    # p.161-165
+    exemplo_livro1 = num.array([2, 4, 7, 4, 8, 5, 5, 2], dtype=int)
+    exemplo_livro2 = num.array([3, 2, 7, 5, 2, 4, 1, 1], dtype=int)
+    exemplo_livro3 = num.array([2, 4, 4, 1, 5, 1, 2, 4], dtype=int)
+    exemplo_livro4 = num.array([3, 2, 5, 4, 3, 2, 1, 3], dtype=int)
+
+    teste =  num.array([5, 1, 4, 2, 6, 1, 4, 7], dtype=int)
+    print(h(entrada=teste))
