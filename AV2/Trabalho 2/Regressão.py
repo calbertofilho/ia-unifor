0d50bc0f6dbfbb86104a96f9f6d57764d30f92d6
Carlos Alberto Filho <calbertofilho@gmail.com>
2024-04-30 22:32:37 -0300

    Update 30-04-2024

--- AV2/Trabalho 2/Regressão.py
+++ AV2/Trabalho 2/Regressão.py
@@ -10,42 +10,46 @@ def get_data() -> pd.DataFrame:
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
     return pd.read_csv("aerogerador.dat", names=["Vel", "Pot"], sep="\t")
 
-def printProgressBar(value, label):
-    n_bar = 40 # tamanho da barra
-    max = 100
-    j = value / max
-    sys.stdout.write('\r')
-    bar = '█' * int(n_bar * j)
-    bar = bar + '-' * int(n_bar * (1 - j))
-    sys.stdout.write(f"{label.ljust(10)} | [{bar:{n_bar}s}] {int(100 * j)}% ")
-    sys.stdout.flush()
-
-def plot(data: pd.DataFrame) -> None:
-    Vel = (data.iloc[:, 0].min(), data.iloc[:, 0].max())
-    Pot = (data.iloc[:, 1].min(), data.iloc[:, 1].max())
-    realm = [Vel, Pot]
-    x_axis = np.linspace(realm[0][0], realm[0][1], 1000)
-    y_axis = np.linspace(realm[1][0], realm[1][1], 1000)
-    plt.plot(x_axis, y_axis)
-    plt.grid()
-    plt.suptitle('Espalhamento dos dados', fontsize = 12)
-    plt.title("Aerogerador")
-    plt.xlabel('Velocidade')
-    plt.ylabel('Potência')
-    for i in range(data.tail(1).index.item() +1):
-        plt.scatter(data.at[i, 'Vel'], data.at[i, 'Pot'], color='green', s=30, marker='o', linewidth=1, edgecolors="black")
-        printProgressBar((i / (data.tail(1).index.item()+1)) * 100, 'Carregando dados')
-    plt.show()
-
 def run() -> None:
-    print(get_data())
+    df = df_shuffled = get_data()
+    # embaralhamento dos dados
+    df_shuffled = df_shuffled.sample(frac=1).reset_index(drop=True)
+    # fatiamento dos dados: 80% ↔ 20%
+    percentual = .8
+    x_treino = df.iloc[:int((df_shuffled.tail(1).index.item()+1)*percentual), 0].values
+    x_treino.shape = (len(x_treino), 1)
+    y_treino = df.iloc[:int((df_shuffled.tail(1).index.item()+1)*percentual), 1].values
+    y_treino.shape = (len(y_treino), 1)
+    x_teste = df.iloc[int((df_shuffled.tail(1).index.item()+1)*percentual):(df.tail(1).index.item()+1), 0].values
+    x_teste.shape = (len(x_teste), 1)
+    y_teste = df.iloc[int((df_shuffled.tail(1).index.item()+1)*percentual):(df.tail(1).index.item()+1), 1].values
+    y_teste.shape = (len(y_teste), 1)
+    # implementação MQO
+    plt.suptitle('Espalhamento dos dados', fontsize = 16)
+    plt.title("Aerogerador")
+    plt.xlabel('Velocidade do vento')
+    plt.ylabel('Potência gerada')
+    plt.grid()
+    # x = df["Vel"].values
+    # x.shape = (len(x), 1)
+    # y = df["Pot"].values
+    # y.shape = (len(y), 1)
+    plt.scatter(x_treino, y_treino, color='green', s=40, marker='o', linewidth=0.4, edgecolors="black", alpha=0.6)
+    X = np.concatenate((np.ones((len(x_treino), 1)), x_treino), axis=1)
+    B = np.linalg.pinv(X.T @ X) @ X.T @ y_treino
+    x_axis = np.linspace(x_treino.min(), x_treino.max(), 100)
+    x_axis.shape = (len(x_axis), 1)
+    ones = np.ones((len(x_axis), 1))
+    X_new = np.concatenate((ones, x_axis), axis=1)
+    Y = X_new @ B
+    plt.plot(x_axis, Y, color='red')
+    plt.show()
 
 def close() -> None:
     sys.exit(0)
 
 try:
     if __name__ == "__main__":
-        plot(get_data())
         run()
 finally:
     close()
